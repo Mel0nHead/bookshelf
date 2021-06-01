0225416677afdf3d4b8c7288badde338afe83116
Alex King <alex.king@deepcrawl.com>
2021-06-01 09:59:43 +0100

    finish unit test exercises

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,5 @@
-test.todo('formatDate formats the date to look nice')
+const {formatDate} = require('utils/misc')
 
+it('formatDate formats the date to the format `mmm yy`', () => {
+  expect(formatDate(1622536237596)).toBe('Jun 21')
+})
