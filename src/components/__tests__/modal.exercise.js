c4266257a43d9526494558961fb449db712ef123
Alex King <alex.king@deepcrawl.com>
2021-06-14 10:09:00 +0100

    start on unit test exercises

--- src/components/__tests__/modal.exercise.js
+++ src/components/__tests__/modal.exercise.js
@@ -1,10 +1,27 @@
-// 🐨 you're gonna need this stuff:
-// import {Modal, ModalContents, ModalOpenButton} from '../modal'
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+import {Modal, ModalContents, ModalOpenButton} from '../modal'
+import userEvent from '@testing-library/user-event'
 
-test.todo('can be opened and closed')
-// 🐨 render the Modal, ModalOpenButton, and ModalContents
-// 🐨 click the open button
-// 🐨 verify the modal contains the modal contents, title, and label
-// 🐨 click the close button
-// 🐨 verify the modal is no longer rendered
-// 💰 (use `query*` rather than `get*` or `find*` queries to verify it is not rendered)
+test('can be opened and closed', () => {
+  render(
+    <Modal>
+      <ModalOpenButton>
+        <button>open modal</button>
+      </ModalOpenButton>
+      <ModalContents aria-label="Modal contents" title="Modal Title">
+        <div>I am modal contents</div>
+      </ModalContents>
+    </Modal>,
+  )
+
+  userEvent.click(screen.getByRole('button', {name: /open modal/i}))
+
+  expect(screen.getByText(/modal title/i)).toBeInTheDocument()
+  expect(screen.getByText(/i am modal contents/i)).toBeInTheDocument()
+
+  userEvent.click(screen.getByRole('button', {name: /close/i}))
+
+  expect(screen.queryByText(/modal title/i)).not.toBeInTheDocument()
+  expect(screen.queryByText(/i am modal contents/i)).not.toBeInTheDocument()
+})
